04f9c5a60d23466a9ccd7d4db68ce234adc4321e
kidokunjane <yoyobaka@gmail.com>
2025-08-13 09:24:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = 'v1.0.11';
+const VERSION = 'v1.0.12';
 // Service Worker が管理できるスコープの先頭URL（例: https://user.github.io/repo/）
 const BASE_URL = self.registration.scope;
 const CACHE_NAME = `mini4wd-race-memo-${VERSION}`;
